4c2eb1e78cf3b97f350e449891b01ac3ea100d65
LaboratorioSmartLAB <Andresfocampoz@gmail.com>
2024-05-03 04:58:13 -0500

    versión3 comp
    animaciones y marcadores de contador conectados y completos

--- escena/lVl2_ALCOBA.gd
+++ escena/lVl2_ALCOBA.gd
@@ -43,3 +43,23 @@ func _on_gato_1_animation_finished():
 func _on_gato_2_animation_finished():
 		$GATO2.visible = false
 		$lestcatchgato2.visible = true
+		
+func _on_cama_area_2d_input_event(viewport, event, shape_idx):
+	if event is InputEventMouseButton and event.button_index == MOUSE_BUTTON_LEFT:
+		$gatocama.play()
+		miaw.play()
+		$imggato4.visible = true
+		
+func _on_gatocama_animation_finished():
+		$gatocama.visible = false
+		$gatocamaidol.visible = true
+		
+func _on_zapato_area_2d_input_event(viewport, event, shape_idx):
+	if event is InputEventMouseButton and event.button_index == MOUSE_BUTTON_LEFT:
+		$gatozapato.play()
+		miaw.play()
+		$imggato5.visible = true
+		
+func _on_gatozapato_animation_finished():
+		$gatozapato.visible = false
+		$gatozapatoidol.visible = true
