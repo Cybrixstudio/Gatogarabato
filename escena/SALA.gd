c81c3353e1759379495041f7412340589ca25912
LaboratorioSmartLAB <Andresfocampoz@gmail.com>
2025-02-01 20:45:43 -0500

    lvl salon sonido

--- escena/SALA.gd
+++ escena/SALA.gd
@@ -1,8 +1,16 @@
 extends Node2D
 	
-@onready var miaw = $miaw
 @onready var piano = $piano
-	
+@onready var miaw7 = $miaw7
+@onready var miaw6 = $miaw
+@onready var miaw8 = $miaw8
+@onready var miaw9 = $miaw9
+@onready var miaw2 = $miaw2
+@onready var equipo = $equipo
+@onready var slibros = $slibros
+@onready var smatera = $smatera
+@onready var ssillon = $ssillon
+
 func _ready():
 	pass 
 func _process(delta):
@@ -16,32 +24,36 @@ func _on_cojin_area_2d_input_event(viewport, event, shape_idx):
 	if event is InputEventMouseButton and event.button_index == MOUSE_BUTTON_LEFT:
 		$gatocojin.play()
 		$imggatocojin.visible = true
-		miaw.play()
+		miaw6.play()
+		ssillon.play()
 
 func _on_area_2d_input_event(viewport, event, shape_idx):
 	if event is InputEventMouseButton and event.button_index == MOUSE_BUTTON_LEFT:
 		$imggatopiano.visible = true
 		$gatopiano.play()
-		miaw.play()
+		miaw7.play()
 		piano.play()
 
 func _on_equipo_area_2d_input_event(viewport, event, shape_idx):
 	if event is InputEventMouseButton and event.button_index == MOUSE_BUTTON_LEFT:
 		$imggatoequipo.visible = true
 		$gatoequipo.play()
-		miaw.play()
+		miaw8.play()
+		equipo.play()
 
 func _on_matera_area_2d_input_event(viewport, event, shape_idx):
 	if event is InputEventMouseButton and event.button_index == MOUSE_BUTTON_LEFT:
 		$imggatomatera.visible = true
 		$gatomatera.play()
-		miaw.play()
+		miaw9.play()
+		smatera.play()
 
 func _on_libros_area_2d_input_event(viewport, event, shape_idx):
 	if event is InputEventMouseButton and event.button_index == MOUSE_BUTTON_LEFT:
 		$imggatolibros.visible = true
 		$gatolibros.play()
-		miaw.play()
+		miaw2.play()
+		slibros.play()
 		
 func _on_gatocojin_animation_finished():
 		$gatocojin.visible = false
